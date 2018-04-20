d735dc8f601da2e84709954d66c241e4a9a9ae26
Vladimir Djukic <vladimir@amerbank.com>
2018-04-20 18:20:03 +0200

    improve di

--- index.ts
+++ index.ts
@@ -1,31 +1,63 @@
-import { Service } from "./Service";
-import { Injector } from "./Injector";
+import { Service } from './Service';
+import { Injector } from './Injector';
 
-@Service()
+
 class FirstClass {
+    private time: any;
+
+    constructor() {
+        this.time = Math.random();
+    }
 
     public method1() {
-        return 'first class';
+        return this.time;
+    }
+}
+
+class FirstClassNew {
+    private time: any;
+
+    constructor() {
+        this.time = Math.random();
+    }
+
+    public method1() {
+        return this.time + "aaaaa";
     }
 }
 
 @Service()
 class SecondClass {
-    constructor(public first: FirstClass) {}
+    constructor(public first: FirstClass) { }
     public method1() {
-        return 'second class'
+        return this.first.method1();
+    }
+}
+
+@Service()
+class SecondClassMock extends SecondClass {
+    
+    public method1() {
+        return 'mocked';
     }
 }
 
 @Service()
 class ThirdClass {
-    constructor(public first: FirstClass, public second: SecondClass) {}
+    constructor(public first: FirstClass, public second: SecondClass) { }
 
     public method3() {
-        return this.first.method1();
+        return this.second.method1();
     }
 }
 
-const third = Injector.resolve(ThirdClass);
+Injector.mock(
+    [
+        {
+            dep: SecondClassMock
+        },
+    ]
+);
 
+const third = Injector.resolve(ThirdClass);
 console.log(third.method3())
