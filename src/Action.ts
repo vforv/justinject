f4e6a4aa180641056f1f0d90672a3b65097deda3
Vladimir Djukic <vladimir@amerbank.com>
2018-04-27 22:31:03 +0200

    fix linit

--- src/Action.ts
+++ src/Action.ts
@@ -6,24 +6,28 @@ export interface IAction {
 export const Action = (pattern: IAction, additionalPattern?: { [key: string]: string }) => {
     return (target: any, key: PropertyKey, descriptor: TypedPropertyDescriptor<any>) => {
         const orginalMethod = descriptor.value;
-        descriptor.value = function (...args: any[]) {
+        descriptor.value = function(...args: any[]) {
 
             const thisClass: any = this;
 
             if (!thisClass.hemera) {
-                throw new Error('Action service must have hemera property. HemeraService must have instance getter which returns Hemera instance.');
+                throw new Error('Action service must have hemera property. \
+                HemeraService must have instance getter which returns Hemera instance.');
             }
-            
+
             if (!thisClass.hemera.instance) {
-                throw new Error('Action service must have hemera property. HemeraService must have instance getter which returns Hemera instance.');
+                throw new Error('Action service must have hemera property. \
+                HemeraService must have instance getter which returns Hemera instance.');
             }
 
             if (!thisClass.validator) {
-                throw new Error('Action service must have validator property. ValidateService must have schema method which returns validation.');
+                throw new Error('Action service must have validator property. \
+                ValidateService must have schema method which returns validation.');
             }
 
             if (!thisClass.validator.schema) {
-                throw new Error('Action service must have validator property. ValidateService must have schema method which returns validation.');
+                throw new Error('Action service must have validator property. \
+                ValidateService must have schema method which returns validation.');
             }
 
             const hemera: any = thisClass.hemera.instance;
